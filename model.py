f52ab26f5b6d46d82f8b5f987849d5f77903aec7
shin-sooin <64197786+shin-sooin@users.noreply.github.com>
2023-05-15 15:44:21 +0900

    v1

--- model.py
+++ model.py
@@ -9,20 +9,26 @@ import logging
 class Model:
 
     def ocr(self, imgUrl):
-        def img_open(img_url):
-            texts = "222"
-            res = ur.urlopen(img_url).read()
-            f = io.BytesIO(res)
-            img = Image.open(f)
-            texts = str(pytesseract.image_to_string(img, lang='kor+eng'))
-            logging.debug(texts)
-            return texts
+        # def img_open(img_url):
+        #     texts = "222"
+        #     res = ur.urlopen(img_url).read()
+        #     f = io.BytesIO(res)
+        #     img = Image.open(f)
+        #     texts = str(pytesseract.image_to_string(img, lang='kor+eng'))
+        #     logging.debug(texts)
+        #     return texts
 
         texts = ""
         error_message = ""
 
         try:
-            texts = img_open(imgUrl)
+            # texts = img_open(imgUrl)
+            texts = "222"
+            res = ur.urlopen(imgUrl).read()
+            f = io.BytesIO(res)
+            img = Image.open(f)
+            texts = str(pytesseract.image_to_string(img, lang='kor+eng'))
+            logging.debug(texts)
         except:
             error_message = "Please check image contains text"
         context = {
